3b1cb8041b10ec88f6aec25a21db34ccdecd4431
Firemission1343 <libertyv13@yahoo.com>
2024-03-11 12:41:15 -0400

    stat component and statistic page

---
+++ client/src/components/stats.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import { ref, computed, inject } from "vue";
+import { type User, getUsers } from "@/model/users";
+import NavBar from "@/components/NavBar.vue";
+
+const users = ref([] as User[]);
+// const myVariable = inject<User | null>('myVariable', null);
+users.value = getUsers();
+
+const currentID = inject("currentID");
+
+const visible = ref(true);
+
+const hideMediaBox = () => {
+  visible.value = false;
+};
+// const myVariable = inject('myVariable.');
+// myVariable.value = { id: 1 }
+</script>
+
+
+<template>
+  <main class="hero  is-large">
+    <div v-if="currentID === -1"> 
+    <h1 class="title">Statistics</h1>
+            <p>Please Login!</p>
+    </div>
+    <div class="container boxes d-flex">
+
+      <div class="columns boxes">
+        <div class="column is-one-quarter">
+          <!-- "Left: " -->
+          
+        </div>
+        <!-- Middle:   -->
+        <div class="column is-half">
+          <div class="colunm  is-flex d-flex"> 
+
+            <div v-if="currentID === 1">
+            <div class="d-flex boxes" v-for="user in users" :key="user.id">
+              <!-- <h1> {{ users }} </h1> -->
+              <div v-if="user.id === 1">
+            <div class="box half-screen">
+              <h2 class="title">Today</h2>
+              <h3 class="subtitle">Workout Type: {{ user.workout.name }} </h3>
+
+              <div class="columns is-multiline boxes">
+
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.sets }}</h3>
+                  <caption class="caption">Sets</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.reps }}</h3>
+                  <caption class="caption">Reps</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.weight }}</h3>
+                  <caption class="caption">Weight</caption>
+                </div>
+                  <div class="column is-half"><h3 class="value">281.5</h3>
+                    <caption class="caption">Calories</caption>
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="box">
+              <h2 class="title">Week</h2>
+              <h3 class="subtitle">Workout Type: {{ user.workout.name }} </h3>
+
+              <div class="columns is-multiline">
+
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.sets }}</h3>
+                  <caption class="caption">Sets</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.reps }}</h3>
+                  <caption class="caption">Reps</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.weight }}</h3>
+                  <caption class="caption">Weight</caption>
+                </div>
+                  <div class="column is-half"><h3 class="value">281.5</h3>
+                    <caption class="caption">Calories</caption>
+                  </div>
+
+                </div>
+              </div>
+
+
+              <div class="box">
+              <h2 class="title">All time</h2>
+              <h3 class="subtitle">Workout Type: {{ user.workout.name }} </h3>
+
+              <div class="columns is-multiline">
+
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.sets }}</h3>
+                  <caption class="caption">Sets</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.reps }}</h3>
+                  <caption class="caption">Reps</caption>
+                </div>
+                <div class="column is-half">
+                  <h3 class="value">{{ user.workout.weight }}</h3>
+                  <caption class="caption">Weight</caption>
+                </div>
+                  <div class="column is-half"><h3 class="value">281.5</h3>
+                    <caption class="caption">Calories</caption>
+                  </div>
+
+                </div>
+              </div>
+
+          </div>
+        </div>
+            </div>
+          </div>
+        </div>
+        <!-- Right:  -->
+        <div class="column is-one-quarter">
+      
+        </div>
+    </div>
+    
+  </div>  
+    
+  </main>
+</template>
+
+<style scoped>
+
+.half-screen {
+  width: 50vw; /* 50% of the viewport width */
+}
+.d-flex {
+  display: flex;
+}
+
+.user {
+  width: 100%; /* Adjust this value as needed */
+}
+
+.flex-column {
+  flex-direction: column;
+}
+
+
+</style>
+
--- client/src/pages/statistics.vue
+++ client/src/pages/statistics.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, computed, inject } from "vue";
 import { type User, getUsers } from "@/model/users";
-import NavBar from "@/components/NavBar.vue";
+import stats from "@/components/stats.vue";
 
 const users = ref([] as User[]);
 
@@ -24,8 +24,10 @@ const hideMediaBox = () => {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css"
     />  
+
   <main class="">
     <div class="container">
+      <stats />
       <div v-if="currentID === 1">
         <div v-for="user in users" :key="user.id">
           <!-- <h1> {{ users }} </h1> -->
@@ -95,6 +97,150 @@ const hideMediaBox = () => {
           </div>
         </div>
       </div>
+
+      <div v-if="currentID === 2">
+        <div v-for="user in users" :key="user.id">
+          <!-- <h1> {{ users }} </h1> -->
+          <div v-if="user.id === 2">
+            <article class="media box" v-if="visible">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img :src="user.thumbnail" alt="" />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>{{ user.firstName }}</strong> &nbsp;
+                    <small>{{ user.handle }}</small> &nbsp;
+                    <small>just now</small>
+                    <br />
+                  </p>
+
+                  <div class="columns">
+                    <div
+                      class="column has-text-centered"
+                      style="
+                        display: flex;
+                        justify-content: space-around;
+                        align-items: center;
+                      "
+                    >
+                      <div>
+                        <div class="title" style="margin: 0px"> {{ user.workout.sets }} </div>
+                        <div class="heading"> Sets
+                        </div>
+                      </div>
+                      <div>
+                        <div class="title" style="margin: 0px">{{ user.workout.reps }}</div>
+                        <div class="heading">Reps</div>
+                      </div>
+                    </div>
+                    <div class="column"></div>
+                  </div>
+                  {{ user.workout.name }} @ {{ user.workout.weight }} lbs
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-reply"></i>
+                      </span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-retweet"></i>
+                      </span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-heart"></i>
+                      </span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+              <div class="media-right">
+                <button class="delete" @click="hideMediaBox"></button>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="currentID === 3">
+        <div v-for="user in users" :key="user.id">
+          <!-- <h1> {{ users }} </h1> -->
+          <div v-if="user.id === 3">
+            <article class="media box" v-if="visible">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img :src="user.thumbnail" alt="" />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>{{ user.firstName }}</strong> &nbsp;
+                    <small>{{ user.handle }}</small> &nbsp;
+                    <small>just now</small>
+                    <br />
+                  </p>
+
+                  <div class="columns">
+                    <div
+                      class="column has-text-centered"
+                      style="
+                        display: flex;
+                        justify-content: space-around;
+                        align-items: center;
+                      "
+                    >
+                      <div>
+                        <div class="title" style="margin: 0px"> {{ user.workout.sets }} </div>
+                        <div class="heading"> Sets
+                        </div>
+                      </div>
+                      <div>
+                        <div class="title" style="margin: 0px">{{ user.workout.reps }}</div>
+                        <div class="heading">Reps</div>
+                      </div>
+                    </div>
+                    <div class="column"></div>
+                  </div>
+                  {{ user.workout.name }} @ {{ user.workout.weight }} lbs
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-reply"></i>
+                      </span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-retweet"></i>
+                      </span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small">
+                        <i class="fas fa-heart"></i>
+                      </span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+              <div class="media-right">
+                <button class="delete" @click="hideMediaBox"></button>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+
+
+
+
     </div>
   </main>
 </template>
