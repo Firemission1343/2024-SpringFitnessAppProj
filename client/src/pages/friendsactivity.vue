058776bd748c8b1ff47e05833b7b58ebc3a1f8ba
firemission1343 <libertyv13@yahoo.com>
2024-04-30 19:09:50 -0400

    fix missing friend

--- client/src/pages/friendsactivity.vue
+++ client/src/pages/friendsactivity.vue
@@ -1,29 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { type User, getUsers } from "@/model/users";
-import { refSession } from '@/viewModel/session';
+
 import FriendActivity from "@/components/FriendActivity.vue";
 
-const session = refSession();
 
-const users = ref([] as User[])
-getUsers()
-    .then((data) => users.value = data.slice(0, 5))
-    .catch((error) => console.error(error));
-;
-
-let isActive = ref(false);
-
-function toggleMenu() {
-  isActive.value = !isActive.value;
- }
-
-const visible = ref(users.value.map(() => true)); //not working as intended
-
-const hideMediaBox = (index: number) => {
-  visible.value[index] = false;
-
-};
 
 </script>
 
@@ -37,23 +16,8 @@ const hideMediaBox = (index: number) => {
       <div class="container">
         <h1 class="title">Friends Activity</h1>
 
-
-          
-
-
-        <!-- <div v-if="session.user?.id === -1">
-          <h1 class="title">login</h1>
-          <p>
-            Please login to view your activity, using the login menu in the top
-            right
-          </p>
-        </div> -->
-
-        <div v-for="user in users" :key="user.id">
-            <div v-if="session.user?.id === user.id">
               <FriendActivity />
-            </div>
-        </div>
+
 
       </div>
     </div>
