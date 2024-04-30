a385f7a7fc223a255ce3498567a37f02a36bff71
firemission1343 <libertyv13@yahoo.com>
2024-04-30 17:14:33 -0400

    Cleanned up

--- client/src/components/FriendActivity.vue
+++ client/src/components/FriendActivity.vue
@@ -1,91 +1,89 @@
 <script setup lang="ts">
 import { ref} from "vue";
 import { type User, getUsers } from "@/model/users";
-import {TheID } from '@/viewModel/user';
+import { type Workout, type UserWorkout, getWorkouts, getUserWorkouts } from "@/model/workouts";
+import { refSession } from '@/viewModel/session';
 
-const users = ref([] as User[]);
- users.value = getUsers();
+const session = refSession();
+
+const users = ref([] as User[])
+
+getUsers()
+        .then((data) => users.value = data.slice(0, 5))
+        .catch((error) => console.error(error));
+    ;
  
+const workouts = ref([] as Workout[])
+getWorkouts()
+    .then((data) => {
+      workouts.value = data.slice(0, 5);
+      session.workout = workouts.value[0];
+    })
+    .catch((error) => console.error(error));
+  
 
-const visible = ref(true);
+const userWorkouts = ref([] as UserWorkout[])
+getUserWorkouts()
+.then((data) => {
+       userWorkouts.value = data;
 
-const hideMediaBox = () => {
-  visible.value = false;
-};
+      console.log(userWorkouts.value); // Log the user workouts to the console for debugging
+
+      return userWorkouts.value;
+    })
+    .catch((error) => console.error(error));
+
+
+// const hideMediaBox = () => {
+//   visible.value = false;
+// };
 
 </script>
 
+
 <template>
   <div>
-    <div v-for="user in users" :key="user.id">
-    <div v-if="user.id !== TheID">
-            
-    <article class="media box" v-if="visible">
-              <figure class="media-left">
-                <p class="image is-64x64">
-                  <img :src="user.thumbnail" alt="" />
-                </p>
-              </figure>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>{{ user.firstName }}</strong> &nbsp;
-                    <small>{{ user.handle }}</small> &nbsp;
-                    <small>just now</small>
-                    <br />
-                  </p>
+    <div v-for="workout in workouts" :key="workout.id">
+      <div v-if="session.user?.id === workout.id">
 
-                  <div class="columns">
-                    <div
-                      class="column has-text-centered"
-                      style="
-                        display: flex;
-                        justify-content: space-around;
-                        align-items: center;
-                      "
-                    >
-                      <div>
-                        <div class="title" style="margin: 0px"> {{ user.workout.sets }} </div>
-                        <div class="heading"> Sets
-                        </div>
-                      </div>
-                      <div>
-                        <div class="title" style="margin: 0px">{{ user.workout.reps }}</div>
-                        <div class="heading">Reps</div>
-                      </div>
-                    </div>
-                    <div class="column"></div>
-                  </div>
-                  {{ user.workout.name }} @ {{ user.workout.weight }} lbs
-                </div>
-                <nav class="level is-mobile">
-                  <div class="level-left">
-                    <a class="level-item">
-                      <span class="icon is-small">
-                        <i class="fas fa-reply"></i>
-                      </span>
-                    </a>
-                    <a class="level-item">
-                      <span class="icon is-small">
-                        <i class="fas fa-retweet"></i>
-                      </span>
-                    </a>
-                    <a class="level-item">
-                      <span class="icon is-small">
-                        <i class="fas fa-heart"></i>
-                      </span>
-                    </a>
-                  </div>
-                </nav>
-              </div>
-              <div class="media-right">
-                <button class="delete" @click="hideMediaBox"></button>
-              </div>
-            </article>
-          </div>
+        <div v-for="(userWorkout, index) in workout.UserWorkout" :key="index">
+
+        <article class="media">
+    <figure class="media-left">
+      <p class="image is-64x64">
+        <img :src="session.user.thumbnail" alt="" />           
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>{{ session.user.firstName }}</strong> <small>@{{ session.user.handle }}</small> <small>just now</small>
+          <br />
+          {{ userWorkout.name }} @ {{ userWorkout.weight }} lbs
+        </p>
+      </div>
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <a class="level-item">
+            <span class="icon is-small"><i class="fas fa-reply"></i></span>
+          </a>
+          <a class="level-item">
+            <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+          </a>
+          <a class="level-item">
+            <span class="icon is-small"><i class="fas fa-heart"></i></span>
+          </a>
+        </div>
+      </nav>
+    </div>
+    <div class="media-right">
+      <button class="delete"></button>
+    </div>
+  </article>
         </div>
       </div>
-
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- client/src/pages/friendsactivity.vue
+++ client/src/pages/friendsactivity.vue
@@ -37,64 +37,24 @@ const hideMediaBox = (index: number) => {
       <div class="container">
         <h1 class="title">Friends Activity</h1>
 
-        <button class="button is-info is-fullwidth" @click="toggleMenu" 
-          :class="{ 'is-active': isActive } ">Add Workout</button>
+
           
-          <form v-show="isActive" action="" >
-              <div class="field">
-                <label class="label">Exercise Name</label>
-                <div class="control">
-                  <input class="input" type="text" placeholder="Exercise Name">
-                </div>
-              </div>
 
-              <div class="field">
-                <label class="label">Sets</label>
-                <div class="control">
-                  <input class="input" type="number" placeholder="Sets">
-                </div>
-              </div>
 
-              <div class="field">
-                <label class="label">Reps</label>
-                <div class="control">
-                  <input class="input" type="number" placeholder="Reps">
-                </div>
-              </div>
-
-              <div class="field">
-                <label class="label">Calories</label>
-                <div class="control">
-                  <input class="input" type="number" placeholder="Calories">
-                </div>
-              </div>
-
-              <div class="field is-grouped">
-                <div class="control">
-                  <button class="button is-link">Submit</button>
-                </div>
-              </div>
-          </form>
-
-        <div v-if="session.user?.id === -1">
+        <!-- <div v-if="session.user?.id === -1">
           <h1 class="title">login</h1>
           <p>
             Please login to view your activity, using the login menu in the top
             right
           </p>
-        </div>
-        <div v-else-if="session.user?.id === 1">
-            <FriendActivity /> 
+        </div> -->
+
+        <div v-for="user in users" :key="user.id">
+            <div v-if="session.user?.id === user.id">
+              <FriendActivity />
+            </div>
         </div>
 
-        <div v-else-if="session.user?.id === 2">
-          <FriendActivity />
-        </div>
-
-        <div v-else-if="session.user?.id === 3">
-           <FriendActivity />
-
-        </div>
       </div>
     </div>
   </main>
